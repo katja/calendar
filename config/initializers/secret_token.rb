acaf1087e0001fc8d17ee3c7416e88124da3d820
katja <ich-bin-sowieso@abwesend.de>
2012-10-26 07:51:36 +0200

    Default Rails files created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendar::Application.config.secret_token = 'a12cbcb6b448da0dfae4478b095079f7998b197a07a377accb95292dd078b459772285702a14ef0c2c40a1bb98922cd76891d4e14f8bf3d14833411e835ba16c'
